bfe6eac3d7504feb31c7c0bcb478eebb70008bfb
aamergb12 <agbelhamidi@gmail.com>
2025-12-01 21:08:30 -0600

    invalid+ws helpers

--- calc.rkt
+++ calc.rkt
@@ -1,1 +1,15 @@
 #lang racket
+
+(define (invalid) (error 'calc "Invalid Expression"))
+
+(define (skip-ws cs)
+  (cond
+    [(null? cs) '()]
+    [(char-whitespace? (car cs)) (skip-ws (cdr cs))]
+    [else cs]))
+
+(define (only-ws? cs)
+  (cond
+    [(null? cs) #t]
+    [(char-whitespace? (car cs)) (only-ws? (cdr cs))]
+    [else #f]))
--- devlog.md
+++ devlog.md
@@ -1,5 +1,11 @@
+1st session
 created everything
 
 made the main files, headers etc
 
 need to add my functions now
+
+2nd session
+adding the validity checker, and whitespace funcs, 
+finished adding invalid as well as the skip and only ws funcs
+
